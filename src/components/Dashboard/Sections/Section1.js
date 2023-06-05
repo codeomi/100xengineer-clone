22717a0082ac545f4389e82583ff479f8cbcc8fc
Omkar Kudale <omkar7890kudale@gmail.com>
2023-06-05 06:59:57 +0530

    Header done

--- src/components/Dashboard/Sections/Section1.js
+++ src/components/Dashboard/Sections/Section1.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./section1.css"
 
 const Section1 = () => {
   return (
-    <div>Section1</div>
+   <>
+   <div className="section-1-container">
+    <div className="logo-container">
+      <img className='logo' src='https://www.100xengineers.com/images/LOD-logo.svg' alt='logo'/>
+    </div>
+    <div className='slider-container'>
+      <div className='silder-button'>Project Based Learning</div>
+      <div className='silder-button'>Project Based Learning</div>
+      <div className='silder-button'>Project Based Learning</div>
+      <div className='silder-button'>Project Based Learning</div>
+      <div className='silder-button'>Project Based Learning</div>
+    </div>
+   </div>
+   </>
   )
 }
 
