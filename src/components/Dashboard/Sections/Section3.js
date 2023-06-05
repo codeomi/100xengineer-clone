ae6447bae2aebc9a1608310f68f01993e826d7f2
Omkar Kudale <omkar7890kudale@gmail.com>
2023-06-05 22:51:58 +0530

    section3 completed

--- src/components/Dashboard/Sections/Section3.js
+++ src/components/Dashboard/Sections/Section3.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import "./section3.css";
 
 function Section3() {
   return (
-    <div>Section3</div>
-  )
+    <>
+      <div className="section3-container">
+        <div className="heading-container">
+          <div className="heading-description text-size-small text-weight-light body-color">
+            PLACEMENT ASSITANCE PROVIDED
+          </div>
+          <div className="heading text-size-large">
+            Our students work at places like
+          </div>
+        </div>
+        <div className="company-names">
+          <div className="company-card-name">
+            <img
+              src="https://www.100xengineers.com/images/ZS_BIG.png"
+              loading="lazy"
+              sizes="126px"
+              alt=""
+              width="126"
+            />
+          </div>
+          <div className="company-card-name">
+            <img
+              src="https://www.100xengineers.com/images/qualcomm-logo-black-and-white.png"
+              loading="lazy"
+              sizes="126px"
+              alt=""
+              width="126"
+            />
+          </div>
+          <div className="company-card-name">
+            <img
+              src="https://www.100xengineers.com/images/jpmorgan-chase-logo-black-and-white.png"
+              loading="lazy"
+              sizes="126px"
+              alt=""
+              width="126"
+            />
+          </div>
+          <div className="company-card-name">
+            <img
+              src="hhttps://www.100xengineers.com/images/oyo.svg"
+              loading="lazy"
+              // sizes="126px"
+              alt=""
+              width="75"
+            />
+          </div>
+        </div>
+        <div className="text-size-regular text-weight-light body-color  font-family-Space margin-top">and many more top companies</div>
+      </div>
+    </>
+  );
 }
 
-export default Section3
+export default Section3;
